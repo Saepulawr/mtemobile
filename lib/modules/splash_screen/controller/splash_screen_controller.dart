95848f8cda1ebddf7ca7980b7d1a143e308b1818
saepulawr <adm.ipul@gmail.com>
2021-09-27 20:02:48 +0700

    clean code

--- lib/modules/splash_screen/controller/splash_screen_controller.dart
+++ lib/modules/splash_screen/controller/splash_screen_controller.dart
@@ -2,8 +2,4 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 
 import 'package:get/get.dart';
 
-class SplashScreenController extends GetxController {
-  var count =
-      0.obs; //jadikan variable count menjadi observer dengan menambahkan .obs
-  void increase() => count++;
-}
+class SplashScreenController extends GetxController {}
